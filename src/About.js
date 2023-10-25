d952b32539a00a09705b1f22d51e856b793982c5
ihunar <info@iskillers.com>
2023-10-25 19:57:32 +0500

    ihunar

---
+++ src/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './About.css';
+const About = () => {
+    return (
+        <div>
+            <h1 id="about-heading">About Page</h1>
+        </div>
+    )
+}
+export default About
---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import './App.css';
+import About from './About';
+import Contact from './Contact';
+const App = () => {
+  const [activePage, setActivePage] = useState("home");
+  console.log("activePage", activePage)
+  // const [name, setName] = useState("iSkillers");
+  // const [email, setEmail] = React.useState("info@gmail.com")
+  // var name = "fsdf"
+  // name = "Fsdfsdfsdf"
+  // const name = "iSkillers";
+  // const updateData = () => {
+  //   alert("updateData")
+  // }
+
+  // const deleteUser = (id) => {
+  //   // alert("deleteUser",id)
+  //   console.log("id", id)
+  // }
+
+  // const UpdateData = () => {
+  //   console.log("1", name)
+  //   name = "iHunar";
+  //   console.log("2", name)
+  // }
+
+
+  // update email address
+  // const updateEmailAddress = () => {
+  //   setEmail("info@ihunar.com")
+  // }
+  return (
+    <div>
+      <button onClick={() => setActivePage("home")}>Home</button>
+      <button onClick={() => setActivePage("about")}>About</button>
+      <button onClick={() => setActivePage("contact")}>Contact</button>
+
+      {/* <p>{name}</p>
+      <button onClick={() => setName("iHunar")}>Update</button>
+
+      <p>{email}</p>
+      <button onClick={() => setEmail("info@iskiller.com")}>Update Email</button>
+      <button onClick={updateEmailAddress}>Update Email</button> */}
+      {/* <button onClick={UpdateData}>Update</button> */}
+      {/* <button onClick={() => alert("iSkillers")}>Update</button>
+      <button onClick={updateData}>Update Data</button>
+      <button onClick={() => deleteUser("45-er-78")}>Delete User</button> */}
+      {activePage === "home" && <h1 id='home-heading'>Home Page</h1>}
+      {activePage === "about" && <About />}
+      {activePage === "contact" && <Contact />}
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+{/* ? */}
+    </div>
+  )
+}
+export default App
