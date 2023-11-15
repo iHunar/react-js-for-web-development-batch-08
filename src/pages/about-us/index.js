34c2dd27c561c8b9a90186ac99afdd72b28b8042
ihunar <info@iskillers.com>
2023-11-15 19:50:06 +0500

    update

--- src/pages/about-us/index.js
+++ src/pages/about-us/index.js
@@ -1,38 +1,101 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Input, Button } from "../../components";
 import Layout from "../../layout";
+import {
+  doc,
+  setDoc,
+  getFirestore,
+  addDoc,
+  collection,
+  updateDoc,
+  getDoc,
+  getDocs,
+  query,
+  where,
+  onSnapshot ,
+  deleteDoc 
+} from "firebase/firestore";
+import firebase from "../../config/firebase";
 const AboutUs = () => {
-  const [name, setName] = useState("");
-  const [phone, setPhone] = useState("");
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
+  const db = getFirestore(firebase);
+  const [name, setName] = useState("Abc");
+  const [phone, setPhone] = useState("12345678910");
+  const [password, setPassword] = useState("132131313");
+  const [email, setEmail] = useState("info@gmail.com");
   const [nameMessage, setNameMessage] = useState("");
   const [phoneMessage, setPhoneMessage] = useState("");
   const [passwordMessage, setPasswordMessage] = useState("");
   const [emailMessage, setEmailMessage] = useState("");
   const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [getDataLoading, setGetDataLoading] = useState(true);
+  const [todos, setTodos] = useState([]);
   // const [edit, setEdit] = useState(false)
+
+  useEffect(() => {
+    let getData = async () => {
+      // const docRef = doc(db, "todos", "q6AEwIelgnkPyUxo6I5I");
+      // const docSnap = await getDoc(docRef);
+      // if (docSnap.exists()) {
+      //   console.log("Document data:", docSnap.data());
+      // } else {
+      //   // docSnap.data() will be undefined in this case
+      //   console.log("No such document!");
+      // }
+
+      // const q = query(collection(db, "todos"));
+      // const querySnapshot = await getDocs(q);
+      // querySnapshot.forEach((doc) => {
+      //   console.log(doc.id, " => ", doc.data());
+      // });
+
+      // const q = query(
+      //   collection(db, "todos")
+      // );
+      // let newTodos = [];
+      // console.log("newTodos >>>>>", newTodos);
+      // const querySnapshot = await getDocs(q);
+      // querySnapshot.forEach((doc) => {
+      //   console.log(doc.id, " => ", doc.data());
+      //   newTodos.push(doc.data());
+      // });
+      // setTodos([...newTodos]);
+      // console.log("newTodos >>>>>", newTodos);
+
+      const q = query(collection(db, "todos"));
+      const unsubscribe = onSnapshot(q, (querySnapshot) => {
+        const newTodos = [];
+        querySnapshot.forEach((doc) => {
+          newTodos.push(doc.data());
+        });
+        setTodos([...newTodos]);
+      });
+
+      setGetDataLoading(false);
+    };
+    getData();
+  }, []);
   const [users, setUsers] = useState([
     {
       name: "User 1",
       phone: "032323",
       email: "email1@gmail.com",
       password: "password",
-      edit: false
+      edit: false,
     },
     {
       name: "User 2",
       phone: "032323",
       email: "email2@gmail.com",
       password: "password",
-      edit: false
+      edit: false,
     },
     {
       name: "User 3",
       phone: "032323",
       email: "email3@gmail.com",
       password: "password",
-      edit: false
+      edit: false,
     },
   ]);
   const [show, setShow] = useState(false);
@@ -44,7 +107,7 @@ const AboutUs = () => {
   const [editName, setEditName] = useState("");
   const [editPhone, setEditPhone] = useState("");
   const [editEmail, setEditEmail] = useState("");
-  const [editPassword, setEditPassword] = useState("")
+  const [editPassword, setEditPassword] = useState("");
   const emailCheck =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   // phone
@@ -86,7 +149,7 @@ const AboutUs = () => {
       setEmailDone(true);
     }
   };
-  const addData = () => {
+  const addData = async () => {
     if (name === "") {
       setNameMessage("Full name required!");
     } else if (phone === "") {
@@ -110,19 +173,30 @@ const AboutUs = () => {
       setPhoneMessage("");
       setPasswordMessage("Password required!");
     } else {
+      setLoading(true);
       setNameMessage("");
       setPhoneMessage("");
       setPasswordMessage("");
       setEmailMessage("");
-      setMessage("Success");
+
       const user = {
         name: name,
         phone: phone,
         email: email,
         password: password,
       };
-      users.push(user);
-      console.log("user", user);
+      // users.push(user);
+      // await setDoc(doc(db, "todos", "LA"), user);
+      const docRef = await addDoc(collection(db, "todos"), user);
+      const updateId = doc(db, "todos", docRef.id);
+      // Set the "capital" field of the city 'DC'
+      await updateDoc(updateId, {
+        id: docRef.id,
+      });
+      // console.log("Document written with ID: ", docRef.id);
+      setMessage("Success");
+      setLoading(false);
+      console.log("user>>>>>>>>", user);
 
       setTimeout(() => {
         setEmail("");
@@ -139,10 +213,10 @@ const AboutUs = () => {
     // setEdit(true)
     // setActiveIndex(index)
     // console.log("user", users[index]);
-    setEditName(users[index].name)
-    setEditPhone(users[index].phone)
-    setEditEmail(users[index].email)
-    setEditPassword(users[index].password)
+    setEditName(users[index].name);
+    setEditPhone(users[index].phone);
+    setEditEmail(users[index].email);
+    setEditPassword(users[index].password);
     for (let i = 0; i < users.length; i++) {
       if (index === i) {
         users[index].edit = true;
@@ -150,9 +224,8 @@ const AboutUs = () => {
         users[i].edit = false;
       }
       setUsers([...users]);
-      console.log("edit>>>>>>> ", users[i].edit)
+      console.log("edit>>>>>>> ", users[i].edit);
     }
-
   };
 
   // delete
@@ -160,8 +233,17 @@ const AboutUs = () => {
     // setActiveIndex(index)
     console.log("user", users[index]);
     users.splice(index, 1);
-    setUsers([...users])
+    setUsers([...users]);
   };
+
+
+
+  // delete todo
+const deleteTodo =async (id)=>{
+console.log("deleteTodo",id)
+await deleteDoc(doc(db, "todos", id));
+// 
+}
   return (
     <Layout username="User 2">
       <h1>About page</h1>
@@ -211,7 +293,8 @@ const AboutUs = () => {
       />
 
       <p style={{ color: "green" }}>{message}</p>
-      <Button title="Add" border={true} onClick={addData} />
+      {/* <Button title={loading ? "Loading ...." : "Add" } border={true} onClick={addData} /> */}
+      <Button title="Add" loading={loading} border={true} onClick={addData} />
 
       <br />
       <br />
@@ -230,6 +313,25 @@ const AboutUs = () => {
           onClick={() => setShow(false)}
         />
       } */}
+      {getDataLoading ? (
+        <p>Loading ....</p>
+      ) : (
+        <div>
+          {todos.length === 0 ? (
+            <p>No data available</p>
+          ) : (
+            todos.map((item, index) => {
+              return (
+                <div key={index} className="card">
+                  <h1>{item.name}</h1>
+                  <button onClick={()=>deleteTodo(item.id)}>Delete</button>
+                </div>
+              );
+            })
+          )}
+        </div>
+      )}
+      <hr />
       <Button
         title={show ? "Hide" : "Show"}
         border={false}
@@ -244,8 +346,7 @@ const AboutUs = () => {
             users.map((item, index) => {
               return (
                 <div key={index} className="card">
-                  {item.edit ?
-
+                  {item.edit ? (
                     <div>
                       <Input
                         title="Full Name"
@@ -254,8 +355,8 @@ const AboutUs = () => {
                         type="text"
                         placeholder="Enter Full Name"
                         name="fullName"
-                      // message={nameMessage}
-                      // icon={false}
+                        // message={nameMessage}
+                        // icon={false}
                       />
                       <Input
                         title="Phone Number"
@@ -264,10 +365,10 @@ const AboutUs = () => {
                         type="number"
                         placeholder="Enter Phone Number"
                         name="phone"
-                      // message={phoneMessage}
-                      // icon={true}
-                      // vStart={phoneVStart}
-                      // isDone={phoneDone}
+                        // message={phoneMessage}
+                        // icon={true}
+                        // vStart={phoneVStart}
+                        // isDone={phoneDone}
                       />
                       <Input
                         title="Email Address"
@@ -276,10 +377,10 @@ const AboutUs = () => {
                         value={editEmail}
                         // onChange={(val) => emailChange(val.target.value)}
                         name="email"
-                      // message={emailMessage}
-                      // icon={true}
-                      // isDone={emailDone}
-                      // vStart={emailVStart}
+                        // message={emailMessage}
+                        // icon={true}
+                        // isDone={emailDone}
+                        // vStart={emailVStart}
                       />
                       <Input
                         title="Password"
@@ -288,15 +389,16 @@ const AboutUs = () => {
                         value={editPassword}
                         // onChange={(val) => setPassword(val.target.value)}
                         name=""
-                      // message={passwordMessage}
-                      // icon={false}
+                        // message={passwordMessage}
+                        // icon={false}
                       />
                       <Button
                         title="Update"
                         border={false}
-                      // onClick={() => editHandler(index)}
+                        // onClick={() => editHandler(index)}
                       />
-                    </div> :
+                    </div>
+                  ) : (
                     <>
                       <p>{item.name}</p>
                       <p>{item.phone}</p>
@@ -323,7 +425,7 @@ const AboutUs = () => {
                         />
                       </div>
                     </>
-                  }
+                  )}
                 </div>
               );
             })
